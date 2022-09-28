4658bef5bb9205dc326e41ebd842fea014e544f9
Alexandre Lima <alexandreatlima@gmail.com>
2022-09-28 15:04:26 -0300

    add cors

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from "express";
 import * as dotenv from "dotenv";
 import { connect } from "./config/db.config.js";
 import { userRouter } from "./routes/user.routes.js";
+import cors from "cors";
 
 dotenv.config();
 connect();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const API_VERSION = "1.0";
