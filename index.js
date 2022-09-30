0c0ec9c63f50f54c8bf24d73c385e455d570aa7f
Alexandre Lima <alexandreatlima@gmail.com>
2022-09-30 14:06:18 -0300

    upload done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import { connect } from "./config/db.config.js";
 import { userRouter } from "./routes/user.routes.js";
+import { uploadImgRouter } from "./routes/uploadImage.routes.js";
 import cors from "cors";
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(express.json());
 const API_VERSION = "1.0";
 
 app.use(`/api/${API_VERSION}/user`, userRouter);
+app.use(`/api/${API_VERSION}/uploadImage`, uploadImgRouter);
 
 app.listen(Number(process.env.PORT), () => {
   console.log(`Server up and running at port ${process.env.PORT}`);
---
+++ routes/uploadImage.routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const uploadImgRouter = express.Router();
+
+uploadImgRouter.post(
+  "/uploadImage",
+  uploadImg.single("picture"),
+  (req, res) => {
+    if (!req.file) {
+      console.log(req.file);
+      return res.status(400).json({ msg: "Upload fail" });
+    }
+
+    return res.status(201).json({ url: req.file.path });
+  }
+);
+
+export { uploadImgRouter };
