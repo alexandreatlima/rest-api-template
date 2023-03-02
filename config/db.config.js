bbdbe3959dea7eefd4760bf3e8711698b0a1e102
Alexandre Lima <81311412+alexandreatlima@users.noreply.github.com>
2023-03-02 16:42:58 -0300

    mongoose strictQuery

--- config/db.config.js
+++ config/db.config.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectToDB() {
   try {
+   mongoose.set("strictQuery", false);
     const dbConnect = await mongoose.connect(process.env.MONGODB_URI);
 
     console.log(`Connected to db: ${dbConnect.connection.name}`);
